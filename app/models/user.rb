798938d0d88e1c7da0f4333341229c1a098cb460
Andres <afre92@gmail.com>
2019-10-09 19:19:12 -0400

    method added to generate data

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,20 @@ class User < ApplicationRecord
   validates :password,
             length: { minimum: 6 },
             if: -> { new_record? || !password.nil? }
+
+
+
+  # assocaite random videos to user passed
+  def associate_random_videos(user_id)
+    100.times do
+      user = User.find(user_id)
+      video = Video.all[rand(0...15)]
+      if user.videos.include?(video)
+        next
+      else
+        user.videos << video
+      end
+    end
+  end
+
 end
